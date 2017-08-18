37aea2b8e6deb08d059c9da32c649fb49022a004
Momchil Tomov <tomov90@gmail.com>
2017-08-18 11:42:55 -0400

    create and augment HMLMDPs

--- saxe2017.m
+++ saxe2017.m
@@ -1,38 +1,54 @@
-minimap = {
-    '$####', ...
-    '.#X..', ...
-    '.....'
-};
+%
+%
+rooms = [
+    'S....#.....';
+    '.....#.....';
+    '..#S.......';
+    '.#...#.S##.';
+    '.....#.....';
+    '#.####X....';
+    '.....###.##';
+    '$.#..#...S.';
+    '..#..#.....';
+    '..S....S#..';
+    '.....#..$..'];
 
-% basis set of tasks
+% single task for LMDP
+%
+minimap = [
+    '$####';
+    '.#X..';
+    '.....'];
+
+% basis set of tasks for MLMDP
 %
 minimaps = {
-    {'1####', ...
-     '.#X..', ...
-     '0...0'}; ...
+    ['1####';
+     '.#X..';
+     '0...0'];
      
-    {'0####', ...
-     '.#X..', ...
-     '1...0'}; ...
+    ['0####';
+     '.#X..';
+     '1...0'];
      
-    {'0####', ...
-     '.#X..', ...
-     '0...1'}; ...
+    ['0####';
+     '.#X..';
+     '0...1'];
 };
 
-
-map = {
-    '###################', ...
-    '#.................#', ...
-    '##.########...#.#.#', ...
-    '#...#.....###.#...#', ...
-    '###...#.#...#.##.##', ...
-    '#.#.###.###.#.#...#', ...
-    '#......X....#.#.#.#', ...
-    '#.###########.#.#.#', ...
-    '#..$.#..........#$#', ...
-    '###################'
-};
+% map from my 3D cannon.js maze
+%
+map = [
+    '###################';
+    '#.................#';
+    '##.########...#.#.#';
+    '#...#.....###.#...#';
+    '###...#.#...#.##.##';
+    '#.#.###.###.#.#...#';
+    '#......X....#.#.#.#';
+    '#.###########.#.#.#';
+    '#..$.#..........#$#';
+    '###################'];
 
 
 L = createLMDP(minimap, 1);
@@ -40,8 +56,93 @@ L = solveLMDP(L);
 sampleLMDP(L, L.a, minimap);
 %sampleLMDP(L, L.P, minimap);
 
-M = createMLMDP(minimaps, 1);
-M = solveMLMDP(M);
+%M = createMLMDP(minimaps, 1);
+%M = solveMLMDP(M);
+
+M = createHMLMDP(rooms, 1);
+
+% Create a hierarchical MLMDP from a maze
+%
+function M = createHMLMDP(map, lambda)
+    % Create basis set of tasks from the reward locations,
+    % where each task = go to a single reward location
+    %
+    maps = {};
+    goals = find(map == '$');
+    subtasks = find(map == 'S');
+    map(subtasks) = '.'; % erase subtask states
+    for g = goals'
+        map(goals) = '0'; % zero out all goals (but keep 'em as goals)
+        map(g) = '$'; % ...except for one
+        maps = [maps; {map}];
+    end
+    map(goals) = '$'; % restore reward states
+    map(subtasks) = 'S'; % restore subtask states
+    assert(~isempty(maps));
+    
+    % Create MLMDP from basis tasks based on reward locations only
+    %
+    M{1} = createMLMDP(maps, lambda);
+    
+    % Augment it based on the subtask locations
+    %
+    M{1} = augmentMLMDP(M{1}, map, lambda);
+    
+    %
+    %
+end
+
+% Augment a MLMDP with subtasks from a maze;
+% helper function for building a hierarchical MLMDP
+%
+function M = augmentMLMDP(M, map, lambda)
+    % Create helper MLMDP whose B corresponds to St
+    %
+    maps = {};
+    goals = find(map == '$');
+    subtasks = find(map == 'S');
+    map(goals) = '.'; % erase all goal states
+    for s = subtasks'
+        map(subtasks) = '0'; % zero out all subtask states (but keep 'em as goals)
+        map(s) = '1'; % ...except for one
+        maps = [maps; {map}];
+    end
+    assert(~isempty(maps));
+    
+    Mt = createMLMDP(maps, lambda);
+    
+    % Augment M with subtask states
+    %
+    Qb = M.Qb;
+    Qt = Mt.Qb;
+    Nt = Mt.Nb; 
+    St = M.N + 1 : M.N + Nt;
+
+    % Augment state space and subtask space
+    %
+    M.St = St;
+    M.Qt = Qt;
+    M.Qb = [Qb zeros(M.Nb, numel(St)); zeros(numel(St), M.Nt) Qt]; % new Qb = [Qb 0; 0; Qt]
+    M.Nt = M.Nt + Nt; % Note: M.Nt != numel(M.St) !!!
+
+    M.S = [M.S, St]; % new S = S union St
+    M.N = M.N + Nt;
+
+    M.B = [M.B, St]; % new B = B union St (!!!)
+    M.Nb = M.Nb + Nt;
+  
+    % Augment passive dynamics
+    %
+    P = [M.P zeros(size(M.P, 1), M.N - size(M.P, 2)); zeros(Nt, M.N)];
+    ind = sub2ind(size(P), St, Mt.B); 
+    P(ind) = 1; % P(subtask state | corresponing square) = 0.5, after normalization
+    P = P ./ sum(P, 1); % normalize
+    P(isnan(P)) = 0; % fix the 0/0's
+    M.P = P;
+    M.Pt = M.P(M.St, :);
+    assert(isequal(M.P(M.St, Mt.B), eye(Nt) * 0.5));
+end
+
 
 % Create a MLMDP from multiple mazes;
 % assumes mazes represent a valid basis task set
@@ -96,9 +197,9 @@ end
 % Initialize an unsolved LMDP from a maze
 %
 function L = createLMDP(map, lambda)
-    state = @(x, y) (x - 1) * numel(map{1}) + (y - 1) + 1;
+    state = @(x, y) sub2ind(size(map), x, y);
     
-    N = numel(map) * numel(map{1}); % number of states
+    N = numel(map); % number of states
     S = 1 : N; % set of states {s}
     B = []; % set of boundary states
     P = zeros(N, N); % passive transitions P(s'|s)
@@ -121,27 +222,27 @@ function L = createLMDP(map, lambda)
 
     % iterate over all states s
     %
-    for x = 1:numel(map)
-        for y = 1:numel(map{1})
+    for x = 1:size(map, 1)
+        for y = 1:size(map, 2)
             s = state(x, y);
-            %fprintf('(%d, %d) --> %d = ''%c''\n', x, y, s, map{x}(y));
+            %fprintf('(%d, %d) --> %d = ''%c''\n', x, y, s, map(x, y));
             assert(ismember(s, S));
             
-            if ismember(map{x}(y), absorbing)
-                if map{x}(y) == '$'
+            if ismember(map(x, y), absorbing)
+                if map(x, y) == '$'
                     R(s) = 10; % $$$
                 else
-                    R(s) = str2num(map{x}(y)); % e.g. 9 = $9
+                    R(s) = str2num(map(x, y)); % e.g. 9 = $9
                 end
                 B = [B, s]; % boundary / absorbing / terminal state
                 continue;
             else
                 R(s) = -1; % time is money
             end
-            if ismember(map{x}(y), agent)
+            if ismember(map(x, y), agent)
                 start = s; % starting state
             end
-            if ~ismember(map{x}(y), passable)
+            if ~ismember(map(x, y), passable)
                 continue; % impassable state
             end
             
@@ -150,15 +251,15 @@ function L = createLMDP(map, lambda)
             for i = 1:size(adj, 1)
                 new_x = x + adj(i, 1);
                 new_y = y + adj(i, 2);
-                if new_x <= 0 || new_x > numel(map) || new_y <= 0 || new_y > numel(map{1})
+                if new_x <= 0 || new_x > size(map, 1) || new_y <= 0 || new_y > size(map, 2)
                     continue % outside the map
                 end
-                if ~ismember(map{new_x}(new_y), passable)
+                if ~ismember(map(new_x, new_y), passable)
                     continue; % impassable state
                 end
                 
                 new_s = state(new_x, new_y);
-                %fprintf('      (%d, %d) --> %d = ''%c''\n', new_x, new_y, new_s, map{new_x}(new_y));
+                %fprintf('      (%d, %d) --> %d = ''%c''\n', new_x, new_y, new_s, map(new_x, new_y));
                 assert(ismember(new_s, S));
                     
                 % passive transition f'n P(new_s|s)
@@ -235,25 +336,23 @@ function sampleLMDP(L, a, map)
     s = L.start;
     r = 0;
     
-    getx = @(s) floor((s - 1) / numel(map{1})) + 1;
-    gety = @(s) mod(s - 1, numel(map{1})) + 1;
+    get_coords = @(s) ind2sub(size(map), s);
     
     agent = 'X';
     empty = '.';
     
-    disp(map');
+    disp(map);
     while ~ismember(s, L.B)
         new_s = samplePF(a(:,s));
         
-        x = getx(s);
-        y = gety(s);
-        new_x = getx(new_s);
-        new_y = gety(new_s);
+        [x, y] = get_coords(s);
+        [new_x, new_y] = get_coords(new_s);
         
-        map{x}(y) = empty;
-        map{new_x}(new_y) = agent;
+        map(x, y) = empty;
+        map(new_x, new_y) = agent;
         
-        disp(map');
+        fprintf('(%d, %d) --> (%d, %d)\n', x, y, new_x, new_y);
+        disp(map);
         s = new_s;
         
         r = r + L.R(s);
